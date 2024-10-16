12dd664855bcf1472994741d8428cccca63a2b8e
Igor Grubel Royes <igor_groyes@hotmail.com>
2024-10-16 16:10:53 -0300

    Adição de camada e classes de business.

---
+++ GrupoAEducacao.Business/Interfaces/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using GrupoAEducacao.Business.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoAEducacao.Business.Interfaces
+{
+    public interface IStudentService
+    {
+        public Task<List<StudentDto>> ListAllStudents();
+        public Task<StudentDto> GetStudentAsync(int id);
+        public Task<int> EditStudentAsync(StudentDto studentDto);
+        public Task<bool> DeleteStudentAsync(int id);
+    }
+}
---
+++ GrupoAEducacao.Business/Services/StudentService.cs
@@ -0,0 +1,82 @@
+﻿using GrupoAEducacao.Business.DTOs;
+using GrupoAEducacao.Business.Interfaces;
+using GrupoAEducacao.Domain.Interfaces;
+using GrupoAEducacao.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoAEducacao.Business.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentService(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<List<StudentDto>> ListAllStudents()
+        {
+            var studentsDtoList = new List<StudentDto>();
+            var studentList = await _studentRepository.ListAllStudents();
+            
+            foreach ( var studentItem in studentList)
+            {
+                var studentDto = MapStudentEntityToDto(studentItem);
+
+                studentsDtoList.Add(studentDto);
+            }
+
+            return studentsDtoList.ToList();
+        }
+
+        public async Task<StudentDto> GetStudentAsync(int id)
+        {
+            var student = await _studentRepository.GetStudentAsync(id);
+
+            var studentDto = MapStudentEntityToDto(student);
+
+            return studentDto;
+        }
+
+        public async Task<int> EditStudentAsync(StudentDto studentDto)
+        {
+            var student = MapStudentDtoToEntity(studentDto);
+
+            return await _studentRepository.EditStudentAsync(student);
+        }
+
+        public async Task<bool> DeleteStudentAsync(int id)
+        {
+            return await _studentRepository.DeleteStudentAsync(id);
+        }
+
+        private StudentDto MapStudentEntityToDto(Student student)
+        {
+            return new StudentDto()
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Email = student.Email,
+                AcademicRegister = student.AcademicRegister,
+                CPF = student.CPF
+            };
+        }
+
+        private Student MapStudentDtoToEntity(StudentDto studentDto)
+        {
+            return new Student()
+            {
+                Id = studentDto.Id,
+                Name = studentDto.Name,
+                Email = studentDto.Email,
+                AcademicRegister = studentDto.AcademicRegister,
+                CPF = studentDto.CPF
+            };
+        }
+    }
+}
